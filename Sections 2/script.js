2c7d7f589b61f3e2cf394dbf5afb35f4b2939413
Vahan Gasparyan <78494973+Vahan-Gyan@users.noreply.github.com>
2021-02-09 15:10:28 +0400

    Add files via upload

---
+++ Sections 2/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+function myFunction() {
+  var x = document.getElementById("myDIV1");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunc() {
+  var x = document.getElementById("myDIV2");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunct() {
+  var x = document.getElementById("myDIV3");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
