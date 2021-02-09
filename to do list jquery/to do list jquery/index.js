f6dc2b13d2d340da9850a4943c8e83e5bc68d356
Vahan Gasparyan <78494973+Vahan-Gyan@users.noreply.github.com>
2021-02-09 15:13:50 +0400

    Add files via upload

---
+++ to do list jquery/to do list jquery/index.js
@@ -0,0 +1,74 @@
+$(document).ready(function() {
+
+
+  // $('SPAN').appendTo().text('X');
+  $('li').append('<span class="close">X</span>');
+  // $('SPAN').toggleClass('.close');
+  $(document).on('click', 'li', function(){
+    $(this).toggleClass('checked')
+  });
+
+  $(document).on('click','.close', function(){
+    $(this).parent().remove();
+  });
+
+  $(document).on('click','.add',  function() {
+  var val = $('#input').val();
+  if (val === '') {
+  alert('Write something please!');
+  }
+  else {
+  $('#ul').append('<li>' + val + '<span class="close">X</span></li>');
+  }
+})
+
+});
+
+
+// var myList = document.getElementsByTagName('LI');
+// var i;
+// for (i = 0; i < myList.length; i++) {
+//  var span = document.createElement('SPAN');
+//  var txt = document.createTextNode('X');
+//  span.className = 'close';
+//  span.appendChild(txt);
+//  myList[i].appendChild(span);
+//  myList[i].onclick = function () {
+//      this.classList.toggle('checked')
+//  }
+// }
+// var close = document.getElementsByClassName('close');
+// var i;
+// for (i = 0; i < myList.length; i++) {
+//     close[i].onclick = function() {
+//         var div = this.parentElement;
+//        div.style.display = 'none';
+//     }
+// }
+// function myFunc() {
+//  var li = document.createElement('li');
+//  var inp = document.getElementById('input').value;
+//  var val = document.createTextNode(inp);
+//  li.appendChild(val);
+//      if (inp === '') {
+//    alert('Write something please!');
+//      }
+//      else {
+//    document.getElementById('ul').appendChild(li);
+//      }
+//  var span = document.createElement('SPAN');
+//  var txt = document.createTextNode('X');
+//  span.className = 'close';
+//  span.appendChild(txt);
+//  li.appendChild(span);
+//    document.getElementById('input').value = '';
+//    li.onclick = function () {
+//        this.classList.toggle('checked')
+//    };
+//  for (i = 0; i < close.length; i++) {
+//    close[i].onclick = function() {
+//      var div = this.parentElement;
+//      div.style.display = "none";
+//    }
+//  }
+// }
